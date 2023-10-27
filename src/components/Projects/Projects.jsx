50210876ed23ba24026348279cca8236bfd756ed
Meriemchm <116971809+Meriemchm@users.noreply.github.com>
2023-10-28 00:43:50 +0100

    Update Projects.jsx

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,7 +5,7 @@ const Projects = () => {
     <div data-aos="fade-up" name="projects" className="md:px-20 mx-auto p-4 flex flex-col  h-full w-full">
           <div className="flex items-center mb-10">
           <p className="text-2xl md:text-4xl text-second font-bold p-2 inline ">
-          Projects
+          Projets
           </p>
 
           <div className="h-0.5 flex-grow bg-second "></div>
