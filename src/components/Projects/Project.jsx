e09370a10b1a4800f6bc54a69a8c5d103a06d991
Meriemchm <116971809+Meriemchm@users.noreply.github.com>
2023-10-28 00:12:46 +0100

    Update Project.jsx

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -30,21 +30,20 @@ const Project = () => {
   };
 
   return (
-    <div className="mx-auto grid grid-cols-1 xl:grid-cols-2 gap-6 text-center py-8 px-0 justify-center items-center ">
+    <div className="mx-auto grid grid-cols-1 xl:grid-cols-1 gap-6 text-center py-8 px-0 justify-center items-center ">
       {projects.map(({ id, src, title, description, hdemo, hcode, pdf }) => {
         return (
           <div
             key={id}
             className=" md:flex shadow-md shadow-gray-600 bg-white rounded-lg"
-            style={{ width: "100%" }}
           >
-            <div className="relative group w-full">
+            <div className="relative group ">
               <img
                 src={src}
                 alt="project-name"
-                className="rounded-md h-52 w-full object-cover duration-200 "
-                style={{ width: "100%" }}
+                className="rounded-md object-cover w-full h-full "
               />
+
               <div
                 onClick={() => togglePDF(pdf)}
                 className="absolute inset-0  bg-black bg-opacity-50 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center justify-center cursor-pointer select-none"
@@ -53,13 +52,13 @@ const Project = () => {
               </div>
             </div>
 
-            <div className="flex flex-col  text-gray-800 mx-5 my-4 justify-between">
+            <div className="flex flex-col flex-grow text-gray-800 mx-5 my-4 justify-between">
               <h2 className="text-bold text-2xl capitalize mb-2 self-start">
                 {title}
               </h2>
               <p className="text-start text-sm">{description}</p>
               <div className="flex justify-end items-end gap-3 mt-4">
-                <button className="flex w1/2 px-6 py-3 duration-200 hover:scale-105">
+                <button className="flex  px-6 py-3 duration-200 hover:scale-105">
                   <a href={hcode} target="_blank" className="flex">
                     Code{" "}
                     <span>
@@ -68,7 +67,7 @@ const Project = () => {
                   </a>
                 </button>
                 {hdemo !== "" && (
-                  <button className="flex w1/2 px-6 py-3 rounded-lg border-[2px] border-gray-200  duration-200 hover:scale-105">
+                  <button className="flex  px-6 py-3 rounded-lg border-[2px] border-gray-200  duration-200 hover:scale-105">
                     <a href={hdemo} target="_blank" className="flex">
                       Demo{" "}
                       <span>
